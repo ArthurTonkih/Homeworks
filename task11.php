58b656d62acaf75aede5e4f60c239523666a4a85
ArthurTonkih <arthur1359@yandex.ru>
2023-04-27 19:24:54 +0600

    task

---
+++ task11.php
@@ -0,0 +1,42 @@
+<?
+// Задача 1.
+function(){
+
+};
+
+// Задача 2.
+$a = 10;
+$b = 5;
+echo $c = $a + $b;
+
+echo "<br>";
+
+// Задача 3.
+$variable = "Hello";
+$Variable = "World";
+echo $variable." ".$Variable;
+
+echo "<br>";
+
+// Задача 4.
+$array = array(1, 2, 3, 4, 5);
+echo $array[4];
+
+echo "<br>";
+
+// Задача 5.
+for ($i = 0; $i < 5; $i++) {
+	echo $i;
+}
+
+echo "<br>";
+
+// Задача 6.
+$a1 = 15;
+if ($a1 == 10) {
+	echo "a is equal to 10";
+} else {
+	echo "a is not equal to 10";
+}
+
+?>
